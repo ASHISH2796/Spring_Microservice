5b876da55b868b502dc0beed3f301c082b144abf
Ashish2796 <ashishgupta2796@gmail.com>
2022-01-23 14:43:54 +0530

    implemented & configured naming server or service registry (eureka server) and client config

---
+++ naming-server/src/test/java/com/ashish/micoservice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.micoservice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
