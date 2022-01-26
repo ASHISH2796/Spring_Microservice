c913e5bec7a58e3647deaec98b9f01525d0b5794
Ashish2796 <ashishgupta2796@gmail.com>
2022-01-26 22:06:25 +0530

    resilience4j config & impl : retry , fallbackmethods

---
+++ currency-exchange-service/src/main/java/com/ashish/microservice/currencyexchangeservice/controller/DemoCircuitBreakerController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.ashish.microservice.currencyexchangeservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+/**
+ * @author Ashish Gupta
+ *
+ */
+@RestController
+public class DemoCircuitBreakerController {
+
+	private Logger log =LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping("/demo-api")
+	//@Retry(name="default")
+	@Retry(name="demo-api",fallbackMethod="defaultfallbackMethod")
+	public String demoMethod() {
+		
+		log.info("inside demoMethod");
+		ResponseEntity<String> response=new RestTemplate().getForEntity("http://localhost:8989/demo", String.class);
+		
+		return response.getBody();
+	}
+	
+	private String defaultfallbackMethod(Exception exc) {
+		return "default fallback response";	
+	}
+}
